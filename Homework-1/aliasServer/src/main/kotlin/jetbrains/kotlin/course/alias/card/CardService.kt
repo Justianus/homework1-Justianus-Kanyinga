30ce5c6d66748b5d1c003f7be3ca3eea85a326d0
Justianus Kanyinga <justianuskanyinga@gmail.com>
2025-04-01 21:42:34 +0200

    Done

--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/card/CardService.kt
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/card/CardService.kt
@@ -1,6 +1,7 @@
 package jetbrains.kotlin.course.alias.card
 
 import jetbrains.kotlin.course.alias.util.IdentifierFactory
+import jetbrains.kotlin.course.alias.util.words
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,14 +11,6 @@ class CardService {
     companion object {
         const val WORDS_IN_CARD = 4
         val identifierFactory: IdentifierFactory = IdentifierFactory()
-        private val words = listOf(
-            "cat", "dog", "house", "car", "tree", "book", "computer", "phone",
-            "table", "chair", "window", "door", "sun", "moon", "star", "cloud",
-            "flower", "grass", "mountain", "river", "ocean", "bird", "fish", "butterfly",
-            "apple", "banana", "orange", "grape", "bread", "milk", "coffee", "tea",
-            "shirt", "pants", "shoes", "hat", "clock", "watch", "pencil", "paper",
-            "music", "movie", "game", "sport", "school", "work", "friend", "family"
-        )
         val cardsAmount = words.size / WORDS_IN_CARD
     }
 
